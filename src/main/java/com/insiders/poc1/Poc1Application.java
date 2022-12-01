9274eccdf362fa236280d60896348b2a0f9b090f
Thyago Lobato <thyagollobato@gmail.com>
2022-12-01 11:46:35 -0300

    Started project and database settings with MySQL and Docker

---
+++ src/main/java/com/insiders/poc1/Poc1Application.java
@@ -0,0 +1,13 @@
+package com.insiders.poc1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc1Application.class, args);
+	}
+
+}
