165dc860574b1c4c6a1d0f93e189d4c511cb8d62
Thyago Lobato <thyagollobato@gmail.com>
2022-12-02 09:45:45 -0300

    Created endpoint to update a customer by id

--- src/main/java/com/insiders/poc1/controller/CustomerController.java
+++ src/main/java/com/insiders/poc1/controller/CustomerController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -53,5 +54,13 @@ public class CustomerController {
     public ResponseEntity<Object> deleteById(@PathVariable Long id){
         customerService.deleteById(id);
         return ResponseEntity.ok().body("Customer deleted successfully!");
+    }
+
+    @PutMapping(value = "/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public CustomerResponseDto update(@PathVariable Long id, @RequestBody CustomerRequestDto customerRequestDto){
+        customerRequestDto.setId(id);
+        customerService.update(customerRequestDto);
+        return mapper.map(customerService.findById(id), CustomerResponseDto.class);
     }
 }
--- src/main/java/com/insiders/poc1/service/CustomerService.java
+++ src/main/java/com/insiders/poc1/service/CustomerService.java
@@ -30,10 +30,23 @@ public class CustomerService {
     public List<Customer> findAll() {
         return customerRepository.findAll();
     }
+
     @Transactional
     public void deleteById(Long id) {
         Optional<Customer> customerOptional = customerRepository.findById(id);
         customerOptional.orElseThrow(() -> new RuntimeException("Customer not found!"));
         customerRepository.deleteById(id);
+    }
+
+    public Customer update(CustomerRequestDto customerRequestDto){
+        if(existById(customerRequestDto.getId())){
+            return customerRepository.save(mapper.map(customerRequestDto, Customer.class));
+        } else {
+            throw new RuntimeException("Customer doesn't exist!");
+        }
+    }
+
+    private boolean existById(Long id) {
+        return customerRepository.existsById(id);
     }
 }
