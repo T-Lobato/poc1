5b87060a4a26d8ff80f2ac1fb356b55715450c3b
Thyago Lobato <thyagollobato@gmail.com>
2022-12-02 13:49:38 -0300

    CAddress Class Creation

---
+++ src/main/java/com/insiders/poc1/entities/Address.java
@@ -0,0 +1,39 @@
+package com.insiders.poc1.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter @Setter
+@Table(name = "tb_address")
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String state;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = false)
+    private String district;
+
+    @Column(nullable = false)
+    private String street;
+
+    @Column(nullable = false)
+    private String number;
+
+    @Column(nullable = false)
+    private String zipCode;
+
+}
