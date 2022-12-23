4bf10bf05068a2b810d6228d4abc108931c8c5f6
Thyago Lobato <thyagollobato@gmail.com>
2022-12-22 21:45:04 -0300

    refact: ActiveProfiles on CustomerControllerTest

--- src/test/java/com/insiders/poc1/controller/CustomerControllerTest.java
+++ src/test/java/com/insiders/poc1/controller/CustomerControllerTest.java
@@ -20,12 +20,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(controllers = CustomerController.class)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class CustomerControllerTest {
 
     @Autowired
